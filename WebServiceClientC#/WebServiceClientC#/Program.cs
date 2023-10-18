06620e8a58c409878ecc0e36af86b9c5e5ec2395
Abdennour Zaim <abdennourzaim1999@gmail.com>
2023-10-18 13:07:46 +0100

    uploading project TP1 to github

---
+++ WebServiceClientC#/WebServiceClientC#/Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+using ServiceReference1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        BanqueServiceClient proxy = new BanqueServiceClient();
+        ConvertRequest request = new ConvertRequest(7600);
+        ConvertResponse response = proxy.Convert(request);
+        Console.WriteLine("le résultat de la conversion : "+response.@return);
+
+        getCompteRequest compteRequest=new getCompteRequest(5);
+        getCompteResponse compteResponse=proxy.getCompte(compteRequest);
+        
+        Console.WriteLine("le code du compte est "+compteResponse.@return.code);
+        Console.WriteLine("le solde de ce compte est "+compteResponse.@return.solde);
+
+
+
+
+
+
+
+
+
+        //  Console.WriteLine("the response is : "+request.ToString());
+    }
+}
